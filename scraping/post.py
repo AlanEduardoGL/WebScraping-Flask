866b6253b936cb8eedf0994d9170cf8c9a3d7e8d
AlanEduardoGL <scorer-cosmo2011@hotmail.com>
2023-08-19 13:47:04 -0600

    Version 0.0.1

--- scraping/post.py
+++ scraping/post.py
@@ -17,8 +17,8 @@ from sqlalchemy.exc import SQLAlchemyError
 bp = Blueprint('post', __name__, url_prefix='/post')
 
 
-# @audit route /post
-@bp.route('/post')
+# @audit route /posts
+@bp.route('/posts')
 @login_required # ! Decorador para requerir la session en esta vista.
 def posts():
     """
@@ -28,7 +28,17 @@ def posts():
         render_template: Muestra la plantilla (admin/posts.html).
     """
     try:
-        posts = Post.query.all()
+        # posts = Post.query.all()
+        posts = {
+            'id_insumo': '1',
+            'id_author_insumo': '1',
+            'title_insumo': 'XBOX',
+            'name_insumo': 'XBOX ONE SERIES S',
+            'price_insumo': '6999',
+            'attributes_insumo': 'XBOX ONE 1 TB ALMACENAMIENTO CARBON EDITION',
+            'url_insumo': 'www.mercadolibre.com/xbox',
+            'created': '2023-12-12'
+        }
     except SQLAlchemyError as e:
         print(f'Error interno al mostrar resultados. Intenta nuevamente mas tarde. Mensaje: {str(e)}')
     else:
